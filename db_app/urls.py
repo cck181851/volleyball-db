0416c2d6899d76c74516c3f7e47287dd22f2ff72
cck181851 <cck181851@gmail.com>
2024-09-08 14:31:55 +0300

    Update urls.py

--- db_app/urls.py
+++ db_app/urls.py
@@ -4,10 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
-    
-    #path('csrf_token/', views.csrf_token, name='csrf_token'),
-    #path('session/', views.session, name='session'),
+    path('login/', views.login, name='login'),    
     path('db_admin_dashboard/', views.db_admin_dashboard, name='db_admin_dashboard'),
     path('player_dashboard/', views.player_dashboard, name='player_dashboard'),
     path('coach_dashboard/', views.coach_dashboard, name='coach_dashboard'),
